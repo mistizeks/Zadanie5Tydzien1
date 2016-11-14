87c9f5fa5de929159f1215cb5461180d83d8005f
Kamil <kamilek6@interia.eu>
2016-11-14 22:15:38 +0100

    Ukończone zadanie 5

---
+++ zad5.js
@@ -0,0 +1,23 @@
+
+var xhr=new XMLHttpRequest();
+
+xhr.open("GET","https://jsonplaceholder.typicode.com/users",true);
+
+xhr.onreadystatechange=function(e){
+    if(this.readyState===4 && this.status ===200)
+        {
+            var users = JSON.parse( this.response );
+
+            var usersList=$("#users");
+
+            $.each(users,function(index,user)
+            {
+                var content= "name: "+user.name+" username: "+user.username+" email: "+user.email;
+                usersList.append('<li>'+content+'</li>') 
+            });
+
+        }
+
+}
+
+xhr.send(null);
