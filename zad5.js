e67a381e591f5a868337fde6de450c670b20b1ea
Kamil <kamilek6@interia.eu>
2016-11-15 21:04:27 +0100

    Pobranie z wykorzystaniem jquery

--- zad5.js
+++ zad5.js
@@ -1,23 +1,34 @@
 
-var xhr=new XMLHttpRequest();
+//var xhr=new XMLHttpRequest();
+//
+//xhr.open("GET","https://jsonplaceholder.typicode.com/users",true);
+//
+//xhr.onreadystatechange=function(e){
+//    if(this.readyState===4 && this.status ===200)
+//        {
+//            var users = JSON.parse( this.response );
+//
+//            var usersList=$("#users");
+//
+//            $.each(users,function(index,user)
+//            {
+//                var content= "name: "+user.name+" username: "+user.username+" email: "+user.email;
+//                usersList.append('<li>'+content+'</li>') 
+//            });
+//
+//        }
+//
+//}
+//
+//xhr.send(null);
 
-xhr.open("GET","https://jsonplaceholder.typicode.com/users",true);
+$.getJSON("https://jsonplaceholder.typicode.com/users",function(users){
+     
+    var usersList=$("#users");
 
-xhr.onreadystatechange=function(e){
-    if(this.readyState===4 && this.status ===200)
-        {
-            var users = JSON.parse( this.response );
-
-            var usersList=$("#users");
-
-            $.each(users,function(index,user)
-            {
-                var content= "name: "+user.name+" username: "+user.username+" email: "+user.email;
-                usersList.append('<li>'+content+'</li>') 
-            });
-
-        }
-
-}
-
-xhr.send(null);
+    $.each(users,function(index,user)
+    {
+        var content= "name: "+user.name+" username: "+user.username+" email: "+user.email;
+        usersList.append('<li>'+content+'</li>') 
+    });
+})
